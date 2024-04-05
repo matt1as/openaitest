736eb34e6443d3554f8a157f9aaad7d313416ae4
Mattias Johansson <mattias.e.johansson@gmail.com>
2024-04-05 17:30:06 +0200

    Added amplify

--- src/pages/api/openapi.ts
+++ src/pages/api/openapi.ts
@@ -1,20 +1,11 @@
 // Rest API that that takes a text field as input and  uses openAI chatgpt4-preview to proofread the it
 import { NextApiRequest, NextApiResponse } from "next";
 import OpenAI from "openai";
-import { ChatCompletionMessageParam, ChatCompletionTool } from "openai/resources/index.mjs";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 
 const openai = new OpenAI();
 
-function getOrderFromSAP(order_id : String ) {
-    if ( order_id == '' ) {
-        return "No order id provided"
-    } else {
-        return "Order status is in progress"
-    }
-
-}
-
-// Function that that takes a text field as input and  uses openAI chatgpt4-preview to proofread the it
+// Function that that takes a text field as input and  uses openAI chatgpt4-preview to proofread it
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
@@ -25,8 +16,8 @@ export default async function handler(
     //OpenAI message string for a helpful language teacher using the type ChatCompletionMessageParam
     const messages : ChatCompletionMessageParam[] = [{
         role: "system",
-        content: `You are a helpful proof reader that will responde in JSON that should look liket this:
-            { "original_text : "xxxxx", "proofread_text": "xxxxx", "errors" : [{"start": 0, "end": 1, "message": "xxxxxx"}]}
+        content: `You are a author that rewrites the text in the style of Garfield. You respond in JSON that should look liket this
+            { "original_text : "xxxxx", "proofread_text": "xxxxx"}]}
         `
         }, 
         {
@@ -34,17 +25,12 @@ export default async function handler(
             content: text,
         }]
 
-
     const completion = await openai.chat.completions.create({
         messages: messages,
-        model: "gpt-3.5-turbo-0125",
+        model: "gpt-4-turbo-preview",
         response_format: {type : "json_object"}
       });
 
-
       res.status(200).send(completion.choices[0].message.content);
-
-
- 
 
 }
--- src/pages/proofreader.tsx
+++ src/pages/proofreader.tsx
@@ -1,27 +1,37 @@
-// Page that takes a text box "text_to_be_read" and calls the api /api/proofread?text=xxxx to proofread the text. The text is presented in a pparagraph below the input box
-import React from "react";
+// Page that takes a text box "text_to_be_read" and calls the api /api/proofread?text=xxxx to proofread the text. The text is presented in a pparagraph below the input box using nextjs components
+import { useState } from "react";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 
+async function callApi(text: string) {
+  const response = await fetch(`/api/openapi?text=` + text);
+  return response.json();
+}
 
 export default function Proofread() {
-    const router = useRouter();
+  const router = useRouter();
   const { register, handleSubmit } = useForm();
+  const [text, setText] = useState("");
 
-  const onSubmit = (data: any) => {
-    const text = data.text_to_be_read;
-    router.push(`/api/proofread?text=${text}`);
+  const onSubmit = async (data: any) => {
+    const text_to_be_read = data.text_to_be_read;
+    router.push(`/proofreader?text=${text_to_be_read}`);
+    const proofreadText = await callApi(text_to_be_read);
+    setText(JSON.stringify(proofreadText.proofread_text));
   };
 
   return (
     <div>
-      <h1>Proofread</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>
-          Text to be proofread:
-          <input type="text" {...register("text_to_be_read")} />
-        </label>
+        <textarea
+          rows={5}
+          placeholder="Enter text to be read"
+          {...register("text_to_be_read")}
+        />
+
+        <button type="submit">Create story as written by garfield</button>
       </form>
+      <p>{text}</p>
     </div>
   );
 }
